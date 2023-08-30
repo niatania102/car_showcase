ec70269c1018c27386b65d83a23eaa2d83171c64
taniasilvana <taniasilvana.lapalelo@wingscorp.com>
2023-08-30 21:25:26 +0700

    develop searchbar with headlessui

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
-import { Hero } from '@/components'
-import Image from 'next/image'
-
+import { CustomFilter, Hero, SearchBar } from "@/components";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       {/* Hero acts as the display to home page to user to give brief information */}
-      <Hero/>
+      <Hero />
+
+      {/* Car catalogue */}
+      <div className="mt-12 padding-x padding-y max-width" id="dicover">
+        <div className="tome__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,13 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-    title: string;
-    containerStyles?: string;
-    handleClick?: MouseEventHandler<HTMLButtonElement>;
-    btnType?: "button" | "submit"; // ? means optional
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit"; // ? means optional
+}
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void; // function accepts manufacturer as a string and doesnt return anything
 }
