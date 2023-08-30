ff8ee2d5e12b4e663af8f77105917a1e6d193986
taniasilvana <taniasilvana.lapalelo@wingscorp.com>
2023-08-30 10:48:52 +0700

    created Hero layout (include footer, header)

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="overflow-hidden">
+      {/* Hero acts as the display to home page to user to give brief information */}
       <Hero/>
     </main>
   )
